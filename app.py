9d6ab68ca4d36987198a2eaabab245f1a0bc0ec6
PARTH_DS <parthvashishtha498@gmail.com>
2026-07-20 20:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,10 @@ demo = gr.Interface(
     theme=gr.themes.Soft()
 )
 
+import os
+
 if __name__ == "__main__":
-    # Render network configuration
-    interface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
